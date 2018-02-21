cc19d79b7739d2b3ed2cfc576b06834476b43e52
watanyu <myloveyoza@gmail.com>
2018-02-21 12:15:46 +0700

    ADD COMPLETE2

--- app/src/main/java/sdu/cs58/watanyu/calculator/MainActivity.java
+++ app/src/main/java/sdu/cs58/watanyu/calculator/MainActivity.java
@@ -12,7 +12,7 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
     ///Explicit
     EditText number1EditText, number2EditText;
-    Button addButton;
+    Button addButton,delButton,PlusButton, DivButton;
     TextView resultTextView;
     int num1,num2, result;
 
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         number1EditText = findViewById(R.id.edtNumber1);
         number2EditText = findViewById(R.id.edtNumber2);
         addButton = findViewById(R.id.btnAdd);
+        delButton = findViewById(R.id.btnDel);
+        PlusButton = findViewById(R.id.btnPlus);
+        DivButton = findViewById(R.id.btnDivide);
         resultTextView = findViewById(R.id.txvResult);
 
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -35,11 +38,43 @@ public class MainActivity extends AppCompatActivity {
                 result = num1 + num2;
 
 
+
                 //เเสดงผลรับการคำนวน
                 resultTextView.setText(result + "");
 
             }
         });//end OnClickListener
+        delButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result = num1 - num2;
+
+                resultTextView.setText(result +"");
+            }
+        });
+
+        PlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result = num1 * num2;
+
+                resultTextView.setText(result + "");
+            }
+        });
+        DivButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result = num1 / num2;
+
+                resultTextView.setText(result + "");
+            }
+        });
 
 
     }//end Method onCreate
