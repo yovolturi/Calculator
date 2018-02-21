acf1d89d7cd6c1a521ee676714f6a252820da6fd
watanyu <myloveyoza@gmail.com>
2018-02-21 11:44:04 +0700

    ADD COMPLETE

--- app/src/main/java/sdu/cs58/watanyu/calculator/MainActivity.java
+++ app/src/main/java/sdu/cs58/watanyu/calculator/MainActivity.java
@@ -2,12 +2,45 @@ package sdu.cs58.watanyu.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    ///Explicit
+    EditText number1EditText, number2EditText;
+    Button addButton;
+    TextView resultTextView;
+    int num1,num2, result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        //2. Initial view ผูกตัวเเฟรบนjava
+        number1EditText = findViewById(R.id.edtNumber1);
+        number2EditText = findViewById(R.id.edtNumber2);
+        addButton = findViewById(R.id.btnAdd);
+        resultTextView = findViewById(R.id.txvResult);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //แปรคาจาก edittextเป็นint
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result = num1 + num2;
+
+
+                //เเสดงผลรับการคำนวน
+                resultTextView.setText(result + "");
+
+            }
+        });//end OnClickListener
+
+
+    }//end Method onCreate
+}//end class
